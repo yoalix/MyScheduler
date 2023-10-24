0efaf64a92f77171e47ff77bb92220396fd7fc94
Yoali <yoali.carachure@gmail.com>
2023-10-24 12:36:35 -0600

    redirect

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,7 +1,12 @@
 import { authorize } from "@/lib/auth/google-auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
 export default async function Login() {
-  await authorize();
+  const client = await authorize();
+  if (client) {
+    redirect("/");
+    return null;
+  }
   return <div>Login</div>;
 }
